ae98b43106f21adae6cd15fd8403b9c6302fb5d4
yn4k4nishi <yn4k4nishi.kit@gmail.com>
2021-07-13 18:33:07 +0900

    画像の追加と画面遷移の実装

--- app/src/main/java/com/example/masuda_switch/MainActivity.java
+++ app/src/main/java/com/example/masuda_switch/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.masuda_switch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setMainScreen();
+    }
+
+    private void setMainScreen(){
         setContentView(R.layout.activity_main);
+
+        Button start_button = findViewById(R.id.start_button);
+        start_button.setOnClickListener(v -> setGameScreen());
+    }
+
+    private void setGameScreen(){
+        setContentView(R.layout.activity_game);
+
+        Button exit_button = findViewById(R.id.exit_button);
+        exit_button.setOnClickListener(v -> setMainScreen());
     }
 }
